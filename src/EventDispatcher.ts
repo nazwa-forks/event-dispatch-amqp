58d42c061dd3d39ada2b8d818e4996791e3bfcd6
Maciej <maciej@moonpay.com>
2022-03-08 15:05:42 +0100

    Eslint warnings

--- src/EventDispatcher.ts
+++ src/EventDispatcher.ts
@@ -1,107 +1,126 @@
-import {defaultMetadataRegistry} from "./MetadataRegistry";
+import { defaultMetadataRegistry } from "./MetadataRegistry";
 
 export class EventDispatcher {
+  // -------------------------------------------------------------------------
+  // Properties
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Properties
-    // -------------------------------------------------------------------------
+  private handlers: {
+    [eventName: string]: { attachedTo: any; callback: (data: any) => void }[];
+  } = {};
 
-    private handlers: { [eventName: string]: { attachedTo: any, callback: ((data: any) => void) }[] } = {};
+  // -------------------------------------------------------------------------
+  // Public Methods
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Public Methods
-    // -------------------------------------------------------------------------
-
-    remove(eventName: string): void;
-    remove(eventNames: string[]): void;
-    remove(callback: (data: any) => void): void;
-    remove(eventNameOrNamesOrCallback: string|string[]|((data: any) => void)): void {
-        if (eventNameOrNamesOrCallback instanceof Array) {
-            eventNameOrNamesOrCallback.forEach(eventName => this.remove(eventName));
-
-        } else if (eventNameOrNamesOrCallback instanceof Function) {
-            Object.keys(this.handlers).forEach(key => {
-                this.handlers[key]
-                    .filter(handler => handler.callback === eventNameOrNamesOrCallback)
-                    .forEach(handler => this.handlers[key].splice(this.handlers[key].indexOf(handler), 1));
-            });
-
-        } else if (typeof eventNameOrNamesOrCallback === "string") {
-            this.handlers[eventNameOrNamesOrCallback] = [];
-        }
+  remove(eventName: string): void;
+  remove(eventNames: string[]): void;
+  remove(callback: (data: any) => void): void;
+  remove(
+    eventNameOrNamesOrCallback: string | string[] | ((data: any) => void)
+  ): void {
+    if (eventNameOrNamesOrCallback instanceof Array) {
+      eventNameOrNamesOrCallback.forEach((eventName) => this.remove(eventName));
+    } else if (eventNameOrNamesOrCallback instanceof Function) {
+      Object.keys(this.handlers).forEach((key) => {
+        this.handlers[key]
+          .filter((handler) => handler.callback === eventNameOrNamesOrCallback)
+          .forEach((handler) =>
+            this.handlers[key].splice(this.handlers[key].indexOf(handler), 1)
+          );
+      });
+    } else if (typeof eventNameOrNamesOrCallback === "string") {
+      this.handlers[eventNameOrNamesOrCallback] = [];
     }
-    
-    detach(detachFrom: any, eventName?: string): void;
-    detach(detachFrom: any, eventNames?: string[]): void;
-    detach(detachFrom: any, callback?: (data: any) => void): void;
-    detach(detachFrom: any, eventNameOrNamesOrCallback?: string|string[]|((data: any) => void)): void {
-        if (eventNameOrNamesOrCallback instanceof Array) {
-            eventNameOrNamesOrCallback.forEach(eventName => this.remove(eventName));
+  }
 
-        } else if (eventNameOrNamesOrCallback instanceof Function) {
-            Object.keys(this.handlers).forEach(key => {
-                this.handlers[key]
-                    .filter(handler => handler.callback === eventNameOrNamesOrCallback)
-                    .forEach(handler => this.handlers[key].splice(this.handlers[key].indexOf(handler), 1));
-            });
-
-        } else if (typeof eventNameOrNamesOrCallback === "string") {
-            const key = eventNameOrNamesOrCallback;
-            this.handlers[key]
-                .filter(handler => handler.attachedTo === detachFrom)
-                .forEach(handler => this.handlers[key].splice(this.handlers[key].indexOf(handler), 1))
-        } else {
-            Object.keys(this.handlers).forEach(key => {
-                this.handlers[key]
-                    .filter(handler => handler.attachedTo === detachFrom)
-                    .forEach(handler => this.handlers[key].splice(this.handlers[key].indexOf(handler), 1));
-            });
-        }
+  detach(detachFrom: any, eventName?: string): void;
+  detach(detachFrom: any, eventNames?: string[]): void;
+  detach(detachFrom: any, callback?: (data: any) => void): void;
+  detach(
+    detachFrom: any,
+    eventNameOrNamesOrCallback?: string | string[] | ((data: any) => void)
+  ): void {
+    if (eventNameOrNamesOrCallback instanceof Array) {
+      eventNameOrNamesOrCallback.forEach((eventName) => this.remove(eventName));
+    } else if (eventNameOrNamesOrCallback instanceof Function) {
+      Object.keys(this.handlers).forEach((key) => {
+        this.handlers[key]
+          .filter((handler) => handler.callback === eventNameOrNamesOrCallback)
+          .forEach((handler) =>
+            this.handlers[key].splice(this.handlers[key].indexOf(handler), 1)
+          );
+      });
+    } else if (typeof eventNameOrNamesOrCallback === "string") {
+      const key = eventNameOrNamesOrCallback;
+      this.handlers[key]
+        .filter((handler) => handler.attachedTo === detachFrom)
+        .forEach((handler) =>
+          this.handlers[key].splice(this.handlers[key].indexOf(handler), 1)
+        );
+    } else {
+      Object.keys(this.handlers).forEach((key) => {
+        this.handlers[key]
+          .filter((handler) => handler.attachedTo === detachFrom)
+          .forEach((handler) =>
+            this.handlers[key].splice(this.handlers[key].indexOf(handler), 1)
+          );
+      });
     }
+  }
 
-    attach(attachTo: any, eventName: string, callback: (data: any) => void): void;
-    attach(attachTo: any, eventNames: string[], callback: (data: any) => void): void;
-    attach(attachTo: any, eventNameOrNames: string|string[], callback: (data: any) => void) {
-        let eventNames: string[] = [];
-        if (eventNameOrNames instanceof Array) {
-            eventNames = <string[]> eventNameOrNames;
-        } else {
-            eventNames = [<string> eventNameOrNames];
-        }
-
-        eventNames.forEach(eventName => {
-            if (!this.handlers[eventName])
-                this.handlers[eventName] = [];
-
-            this.handlers[eventName].push({ attachedTo: attachTo, callback: callback });
-        });
-    }
-
-    on(eventName: string, callback: (data: any) => void): void;
-    on(eventNames: string[], callback: (data: any) => void): void;
-    on(eventNameOrNames: string|string[], callback: (data: any) => void) {
-        this.attach(undefined, <any> eventNameOrNames, callback);
-    }
-
-    dispatch(eventName: string, data?: any): void;
-    dispatch(eventNames: string[], data?: any): void;
-    dispatch(eventNameOrNames: string|string[], data?: any) {
-        let eventNames: string[] = [];
-        if (eventNameOrNames instanceof Array) {
-            eventNames = <string[]> eventNameOrNames;
-        } else if (typeof eventNameOrNames === "string") {
-            eventNames = [eventNameOrNames];
-        }
-
-        eventNames.forEach(eventName => {
-            if (this.handlers[eventName])
-                this.handlers[eventName].forEach(handler => handler.callback(data));
-            
-            defaultMetadataRegistry
-                .collectEventsHandlers
-                .filter(handler => handler.hasOwnProperty(eventName))
-                .forEach(handler => handler[eventName](data));
-        });
+  attach(attachTo: any, eventName: string, callback: (data: any) => void): void;
+  attach(
+    attachTo: any,
+    eventNames: string[],
+    callback: (data: any) => void
+  ): void;
+  attach(
+    attachTo: any,
+    eventNameOrNames: string | string[],
+    callback: (data: any) => void
+  ) {
+    let eventNames: string[] = [];
+    if (eventNameOrNames instanceof Array) {
+      eventNames = <string[]>eventNameOrNames;
+    } else {
+      eventNames = [<string>eventNameOrNames];
     }
 
+    eventNames.forEach((eventName) => {
+      if (!this.handlers[eventName]) this.handlers[eventName] = [];
+
+      this.handlers[eventName].push({
+        attachedTo: attachTo,
+        callback: callback,
+      });
+    });
+  }
+
+  on(eventName: string, callback: (data: any) => void): void;
+  on(eventNames: string[], callback: (data: any) => void): void;
+  on(eventNameOrNames: string | string[], callback: (data: any) => void) {
+    this.attach(undefined, <any>eventNameOrNames, callback);
+  }
+
+  dispatch(eventName: string, data?: any): void;
+  dispatch(eventNames: string[], data?: any): void;
+  dispatch(eventNameOrNames: string | string[], data?: any) {
+    let eventNames: string[] = [];
+    if (eventNameOrNames instanceof Array) {
+      eventNames = <string[]>eventNameOrNames;
+    } else if (typeof eventNameOrNames === "string") {
+      eventNames = [eventNameOrNames];
+    }
+
+    eventNames.forEach((eventName) => {
+      if (this.handlers[eventName])
+        this.handlers[eventName].forEach((handler) => handler.callback(data));
+
+      defaultMetadataRegistry.collectEventsHandlers
+        // eslint-disable-next-line no-prototype-builtins
+        .filter((handler) => handler.hasOwnProperty(eventName))
+        .forEach((handler) => handler[eventName](data));
+    });
+  }
 }
--- src/MetadataRegistry.ts
+++ src/MetadataRegistry.ts
@@ -1,81 +1,91 @@
-import {SubscriberMetadata} from "./SubscriberMetadata";
-import {EventSubscriberInterface} from "./EventSubscriberInterface";
-import {OnMetadata} from "./OnMetadata";
-import {EventsHandler} from "./EventsHandler";
+import { EventsHandler } from "./EventsHandler";
+import { EventSubscriberInterface } from "./EventSubscriberInterface";
+import { OnMetadata } from "./OnMetadata";
+import { SubscriberMetadata } from "./SubscriberMetadata";
 
 /**
  * Registry for all controllers and actions.
  */
 export class MetadataRegistry {
+  // -------------------------------------------------------------------------
+  // Properties
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Properties
-    // -------------------------------------------------------------------------
+  private _container?: { get(someClass: any): any };
+  private _collectEventsHandlers: SubscriberMetadata[] = [];
+  private _onMetadatas: OnMetadata[] = [];
 
-    private _container: { get(someClass: any): any };
-    private _collectEventsHandlers: SubscriberMetadata[] = [];
-    private _onMetadatas: OnMetadata[] = [];
+  // -------------------------------------------------------------------------
+  // Accessors
+  // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Accessors
-    // -------------------------------------------------------------------------
+  /**
+   * Sets a container that can be used in subscribers. This allows you to inject container services into
+   * subscribers.
+   */
+  set container(container: { get(someClass: any): any }) {
+    this._container = container;
+  }
 
-    /**
-     * Sets a container that can be used in subscribers. This allows you to inject container services into
-     * subscribers.
-     */
-    set container(container: { get(someClass: any): any }) {
-        this._container = container;
+  /**
+   * Gets all events handlers that registered here via annotations.
+   */
+  get collectEventsHandlers(): EventsHandler[] {
+    return this._collectEventsHandlers.reduce(
+      (handlers: EventsHandler[], subscriber: SubscriberMetadata) => {
+        const instance: EventSubscriberInterface | undefined =
+          this.instantiateClass(subscriber);
+        if (instance && instance.subscribedTo)
+          handlers.push(instance.subscribedTo());
+
+        this._onMetadatas
+          .filter(
+            (metadata) => metadata.object.constructor === subscriber.object
+          )
+          .forEach((metadata) =>
+            metadata.eventNames.map((eventName) => {
+              handlers.push({
+                [eventName]: (data: any) =>
+                  (<any>instance)[metadata.methodName](data),
+              });
+            })
+          );
+
+        return handlers;
+      },
+      []
+    );
+  }
+
+  // -------------------------------------------------------------------------
+  // Adder Methods
+  // -------------------------------------------------------------------------
+
+  addSubscriberMetadata(metadata: SubscriberMetadata) {
+    this._collectEventsHandlers.push(metadata);
+  }
+
+  addOnMetadata(metadata: OnMetadata) {
+    this._onMetadatas.push(metadata);
+  }
+
+  // -------------------------------------------------------------------------
+  // Private Methods
+  // -------------------------------------------------------------------------
+
+  private instantiateClass(subscriber: SubscriberMetadata) {
+    if (!subscriber.instance) {
+      const cls: any = subscriber.object;
+      subscriber.instance = this._container
+        ? this._container.get(cls)
+        : new cls();
     }
 
-    /**
-     * Gets all events handlers that registered here via annotations.
-     */
-    get collectEventsHandlers(): EventsHandler[] {
-        return this._collectEventsHandlers
-            .reduce((handlers: EventsHandler[], subscriber: SubscriberMetadata) => {
-                let instance: EventSubscriberInterface = this.instantiateClass(subscriber);
-                if (instance.subscribedTo)
-                    handlers.push(instance.subscribedTo());
-
-                this._onMetadatas
-                    .filter(metadata => metadata.object.constructor === subscriber.object)
-                    .forEach(metadata => metadata.eventNames.map(eventName => {
-                        handlers.push({ [eventName]: (data: any) => (<any> instance)[metadata.methodName](data) });
-                    }));
-
-                return handlers;
-            }, []);
-    }
-
-    // -------------------------------------------------------------------------
-    // Adder Methods
-    // -------------------------------------------------------------------------
-
-    addSubscriberMetadata(metadata: SubscriberMetadata) {
-        this._collectEventsHandlers.push(metadata);
-    }
-
-    addOnMetadata(metadata: OnMetadata) {
-        this._onMetadatas.push(metadata);
-    }
-
-    // -------------------------------------------------------------------------
-    // Private Methods
-    // -------------------------------------------------------------------------
-
-    private instantiateClass(subscriber: SubscriberMetadata) {
-        if (!subscriber.instance) {
-            const cls: any = subscriber.object;
-            subscriber.instance = this._container ? this._container.get(cls) : new cls();
-        }
-
-        return subscriber.instance;
-    }
-
+    return subscriber.instance;
+  }
 }
 
 /**
  * Default action registry is used as singleton and can be used to storage all metadatas.
  */
-export let defaultMetadataRegistry = new MetadataRegistry();
+export const defaultMetadataRegistry = new MetadataRegistry();
